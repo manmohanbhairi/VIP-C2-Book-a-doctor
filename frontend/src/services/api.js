73bee201fe0c1f4cd6cd17a87bb9e7fe339e5895
Bhairi Manmohan <manmohanbhairi@gmail.com>
2026-06-13 23:55:57 +0530

    Connected frontend with deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+
+baseURL:
+"https://vip-c2-book-a-doctor.onrender.com/api"
+
 });
+
 
 export default API;
